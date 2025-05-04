c12327c9b1493150032a325292dc9104052d72cf
User000811 <buquicchio.vincenzo.28082009@iissvoltadegemmis.edu.it>
2025-05-04 14:19:03 +0200

    Add files via upload

---
+++ progetto/progetto.py
@@ -0,0 +1,21 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>pagina base</title>
+
+<style>
+        body{
+            font-family: Arial, sans-serif;
+            background-image:url("background.jpg")
+            }
+</style>
+</head>
+
+<body>
+    
+</body>
+
+</html>
